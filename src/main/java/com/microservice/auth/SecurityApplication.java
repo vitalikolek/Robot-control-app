0cbe616d3fb6b95e7e770a12dd056deb7c5292c9
Vitalii Oleksenko <103362692+vitalikolek@users.noreply.github.com>
2024-04-25 17:51:53 +0200

    delete swagger

--- src/main/java/com/microservice/auth/SecurityApplication.java
+++ src/main/java/com/microservice/auth/SecurityApplication.java
@@ -1,14 +1,10 @@
 package com.microservice.auth;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Authentication APIs"))
 public class SecurityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
--- src/main/java/com/microservice/auth/config/JwtAuthenticationFilter.java
+++ src/main/java/com/microservice/auth/config/JwtAuthenticationFilter.java
@@ -46,10 +46,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			@NonNull HttpServletResponse response,
 			@NonNull FilterChain filterChain
 	) throws ServletException, IOException {
-			if (request.getServletPath().contains(authBaseUrl)) {
-			filterChain.doFilter(request, response);
-			return;
-		}
+
 		if (request.getServletPath().contains(authBaseUrl)) {
 			filterChain.doFilter(request, response);
 			return;
